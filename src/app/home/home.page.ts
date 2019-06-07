ae41c1a541aaf192cebd38d9247afff0c10b18af
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2019-06-07 11:22:01 +0100

    Login page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public email: string = "";
+  public password: string = "";
 
-  constructor() {}
+  constructor(private httpClient: HttpClient, private navCtrl: NavController) {}
 
+  login() {
+    const authReq = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.httpClient.post("http://localhost:3000/api/auth", authReq).subscribe(
+      (response: any) => {
+        const userId = response.id;
+        // Navigate
+        this.navCtrl.navigateForward("profile", {
+          queryParams: {
+            user_id: userId
+          }
+        });
+      },
+      err => {
+        alert("Failed to login");
+      }
+    );
+  }
 }
